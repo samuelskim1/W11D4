3706c383dcc0ed0a485c186a581cc51380172709
Kelsang <kelsangrtsering@gmail.com>
2023-03-23 15:33:19 -0400

    make save

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
@@ -1,10 +1,19 @@
-// export const 
+import produceData from "../mockData/produce.json"
+const POPULATE = 'produce/POPULATE'
 
+export const populateProduce = produceData => {
+    return {
+        type: POPULATE,
+        produce: produceData
+    }
+}
 
 export default function produceReducer(state = {}, action) {
     const nextState = Object.assign({}, Object.freeze(state));
 
     switch (action.type) {
+        case POPULATE:
+            nextState[action.p]
         default:
             return state;
     }
